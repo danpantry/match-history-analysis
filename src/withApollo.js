0bc2d3e80f910ec5f29b60bb933716ce9dd3f13c
Dan Pantry <devnull@danpantry.me>
2017-05-19 17:58:08 +0100

    use es6 imports for conditionals in withApollo
    This will be eliminated by dead code if it's not required

--- src/withApollo.js
+++ src/withApollo.js
@@ -5,14 +5,12 @@ import {
   ApolloProvider,
   ApolloClient
 } from 'react-apollo';
+import { MockedProvider } from 'react-apollo/lib/test-utils';
+import mocks from './mocks';
 
 const useMocks = process.env.NODE_ENV === 'development' || 'test';
-
 export default function withApollo(component) {
   if (useMocks) {
-    console.info('[apollo]', 'using mocks');
-    const { MockedProvider } = require('react-apollo/lib/test-utils');
-    const mocks = require('./mocks').default;
     return <MockedProvider mocks={mocks}>
       {component}
     </MockedProvider>;
